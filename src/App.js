a65ed17bdadb4428bababf419201a31244efe103
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-05-11 16:56:19 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UnderConstruction from './components/UnderConstruction';
+
+function App() {
+  return (
+    <div className="App">
+      <UnderConstruction />
+    </div>
+  );
+}
+
+export default App;
