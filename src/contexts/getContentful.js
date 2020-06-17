1233e8988992752d5b6ee137a858e4ba800cc831
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-06-17 15:29:50 +0300

    revise credential

---
+++ src/contexts/getContentful.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+    space: process.env.REACT_APP_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+});
+
+export default client;
