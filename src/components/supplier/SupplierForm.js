02bb6786d1f4c9e29449e37e0805babc898d6d7c
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-07-02 16:44:59 +0300

    t Revert "add email"
    This reverts commit 248aa1c32c376c0f7ad32b9d761f679626e15392.

--- src/components/supplier/SupplierForm.js
+++ src/components/supplier/SupplierForm.js
@@ -5,44 +5,39 @@ import { FirebaseContext } from '../../contexts/firebase';
 import Button from '../share/Button';
 
 const SupplierForm = () => {
-    const { register,  errors } = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const [formData, setFormData] = useState();
     const [error, setError] = useState(false);
     const [message, setMessage] = useState();
+    const firebase = useContext(FirebaseContext);
 
-    const encode = (data) => {
-        return Object.keys(data)
-            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-            .join("&");
-      }
-    
-    const  handleSubmit = (e) => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...formData })
-        })
-          .then(() => alert("Success!"))
-          .catch(error => alert(error));
-  
-        e.preventDefault();
-      };
-  
-    const  handleChange = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value });
+    const sendEmail = () => {
+        Axios.post(
+            'https://us-central1-kielowebsite.cloudfunctions.net/submit',
+            formData,
+        )
+            .then((res) => {
+                if (res.status === 200) {
+                    firebase.cloudDB.collection('emails').add(formData)
+                        .then(res => setMessage("Form submited successfully"))
+                }
+            })
+            .catch(err => setError("Form failed to sent. Please submit again"))
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        // setFormData(data);
+    const onSubmit = (data) => {
+        // e.preventDefault();
+        setFormData(data);
+        console.log(data);
+        sendEmail();
     };
-
+  
     return (
         <div className='bg-light py-lg-5'>
             <div className='container p-3 pd-5 bg-light'>
                 <h2 className='text-center my-3 mt-md-5'>Supplier Form</h2>
                 <p className='text-center mb-3 mb-md-5'>Please complete the following form if you are interested to be our supplier partner.</p>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-row">
                         <div className="form-group col-12 col-md-6">
                             <label htmlFor="company">Company</label>
@@ -51,13 +46,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="company"
                                 name="company"
-                                onChange={handleChange}
-                            // ref={register({
-                            //     required: "Company is required",
-                            //     maxLength: { value: 100, message: "Max 100 characters" },
-                            //     minLength: { value: 3, message: "Min 3 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Company is required",
+                                    maxLength: { value: 100, message: "Max 100 characters" },
+                                    minLength: { value: 3, message: "Min 3 characters" }
+                                })} />
                             {errors.company && <p className='error-message'>{errors.company.message}</p>}
                         </div>
                         <div className="form-group col-12 col-md-6">
@@ -67,12 +60,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="address"
                                 name="address"
-                            // ref={register({
-                            //     required: "Address is required",
-                            //     maxLength: { value: 200, message: "Max 200 characters" },
-                            //     minLength: { value: 10, message: "Min 10 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Address is required",
+                                    maxLength: { value: 200, message: "Max 200 characters" },
+                                    minLength: { value: 10, message: "Min 10 characters" }
+                                })} />
                             {errors.address && <p className='error-message'>{errors.address.message}</p>}
                         </div>
                     </div>
@@ -84,12 +76,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="taxNumber"
                                 name="taxNumber"
-                            // ref={register({
-                            //     required: "Tax number is required",
-                            //     maxLength: { value: 100, message: "Max 100 characters" },
-                            //     minLength: { value: 3, message: "Min 3 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Tax number is required",
+                                    maxLength: { value: 100, message: "Max 100 characters" },
+                                    minLength: { value: 3, message: "Min 3 characters" }
+                                })} />
                             {errors.taxNumber && <p className='error-message'>{errors.taxNumber.message}</p>}
 
                         </div>
@@ -100,12 +91,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="businessLicense"
                                 name="businessLicense"
-                            // ref={register({
-                            //     required: "Business license is required",
-                            //     maxLength: { value: 100, message: "Max 100 characters" },
-                            //     minLength: { value: 3, message: "Min 3 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Business license is required",
+                                    maxLength: { value: 100, message: "Max 100 characters" },
+                                    minLength: { value: 3, message: "Min 3 characters" }
+                                })} />
                             {errors.businessLicense && <p className='error-message'>{errors.businessLicense.message}</p>}
                         </div>
                     </div>
@@ -117,12 +107,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="contactPerson"
                                 name="contactPerson"
-                            // ref={register({
-                            //     required: "Contact person is required",
-                            //     maxLength: { value: 100, message: "Max 100 characters" },
-                            //     minLength: { value: 3, message: "Min 3 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Contact person is required",
+                                    maxLength: { value: 100, message: "Max 100 characters" },
+                                    minLength: { value: 3, message: "Min 3 characters" }
+                                })} />
                             {errors.contactPerson && <p className='error-message'>{errors.contactPerson.message}</p>}
                         </div>
                         <div className="form-group col-12 col-md-6">
@@ -132,12 +121,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="contactPosition"
                                 name="contactPosition"
-                            // ref={register({
-                            //     required: "Position is required",
-                            //     maxLength: { value: 100, message: "Max 100 characters" },
-                            //     minLength: { value: 3, message: "Min 3 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Position is required",
+                                    maxLength: { value: 100, message: "Max 100 characters" },
+                                    minLength: { value: 3, message: "Min 3 characters" }
+                                })} />
                             {errors.contactPosition && <p className='error-message'>{errors.contactPosition.message}</p>}
                         </div>
                     </div>
@@ -149,13 +137,13 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="email"
                                 name="email"
-                            // ref={register({
-                            //     required: "Email is required",
-                            //     pattern: {
-                            //         value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                            //         message: "Please enter a valid email"
-                            //     }
-                            // })}
+                                ref={register({
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: "Please enter a valid email"
+                                    }
+                                })}
                             />
                             {errors.email && <p className='error-message'>{errors.email.message}</p>}
                         </div>
@@ -166,12 +154,11 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="phone"
                                 name="phone"
-                            // ref={register({
-                            //     required: "Phone number is required",
-                            //     maxLength: { value: 20, message: "Max 12 characters" },
-                            //     minLength: { value: 6, message: "Min 8 characters" }
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Phone number is required",
+                                    maxLength: { value: 20, message: "Max 12 characters" },
+                                    minLength: { value: 6, message: "Min 8 characters" }
+                                })} />
                             {errors.phone && <p className='error-message'>{errors.phone.message}</p>}
                         </div>
                     </div>
@@ -183,10 +170,9 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="productRange"
                                 name="productRange"
-                            // ref={register({
-                            //     required: "Product range is required"
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Product range is required"
+                                })} />
                             {errors.productRange && <p className='error-message'>{errors.productRange.message}</p>}
                         </div>
                         <div className="form-group col-12 col-md-6">
@@ -196,10 +182,9 @@ const SupplierForm = () => {
                                 className="form-control"
                                 id="capacity"
                                 name="capacity"
-                            // ref={register({
-                            //     required: "Product range is required"
-                            // })} 
-                            />
+                                ref={register({
+                                    required: "Product range is required"
+                                })} />
                             {errors.capacity && <p className='error-message'>{errors.capacity.message}</p>}
                         </div>
                     </div>
@@ -233,10 +218,9 @@ const SupplierForm = () => {
                             className="form-control-file"
                             name="catalog"
                             id="catalog"
-                        // ref={register({
-                        //     required: "This is required"
-                        // })} 
-                        />
+                            ref={register({
+                                required: "This is required"
+                            })} />
                         {errors.catalog && <p className='error-message'>{errors.catalog.message}</p>}
                     </div>
                     <div className="form-group">
