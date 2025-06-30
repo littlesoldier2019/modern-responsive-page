b5ac84562f253f280ee34b2891fb901798dff723
Helmi Nguyen <helmi.nguyen@smartly.io>
2025-06-30 17:12:18 +0300

    use variables

---
+++ src/assets/copy/variables.ts
@@ -0,0 +1,6 @@
+export const variables = {
+  address: "Moisionkatu 3 A 08100, Lohja, Finland",
+  email: "lydia@kielo.co",
+  phone: "+358 4131 113 852",
+  facebook: "www.facebook.com/kieloconsultancy/",
+};
